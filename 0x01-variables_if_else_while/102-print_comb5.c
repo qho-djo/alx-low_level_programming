1420fb5286ee699ce50e16456eb6b1efc5cff1ec
Kenny Frempong <brakojo21@gmail.com>
2023-07-17 10:14:57 -0700

    Comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,14 +9,27 @@
 */
 int main(void)
 {
-	int n;
+	int num1, num2;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	int num1;
-	int num2;
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-	for (num1 = 0; num1 < 100; num1++)
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
