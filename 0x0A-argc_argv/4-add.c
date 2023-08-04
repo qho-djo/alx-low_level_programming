745c7ba7e2d84795d7f53fde4f6ae80b3bc262c0
Kenny Frempong <brakojo21@gmail.com>
2023-08-03 19:53:02 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers, followed by a new line.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: 0 on success, print 'Error' and return 1 on failure.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+	sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -4,3 +4,4 @@ Silence is argument carried out by other means
 Silence is argument carried out by other means
 The best argument against democracy is a five-minute conversation with the average voter
 Neither irony nor sarcasm is argument
+To infinity and beyond
