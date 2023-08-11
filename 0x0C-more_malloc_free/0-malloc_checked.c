a94bf2b5243fc066c8d847d5541afa363ae93b2b
Kenny Frempong <brakojo21@gmail.com>
2023-08-11 06:10:54 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: unsigned integer.
+ *
+ * Return: 98 upon failure.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = (int *)malloc(sizeof(int) * b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
