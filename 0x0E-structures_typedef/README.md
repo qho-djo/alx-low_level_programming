c44ec1e0fcab19128c8ef5e6101c4659dd48eb09
Kenny Frempong <brakojo21@gmail.com>
2023-08-14 07:16:21 -0700

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -5,3 +5,4 @@ Poppy
 Poppy
 A dog is the only thing on earth that loves you more than you love yourself
 Betty is happy
+A dog will teach you unconditional love. If you can have that in your life, things won't be too bad
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - a new type for dogs with elements.
@@ -16,5 +19,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
